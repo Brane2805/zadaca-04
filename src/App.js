520058967d1a07055fe7982cb06652b34d285869
Branimir Matijacic <brane2805@gmail.com>
2021-12-18 21:27:23 +0100

    gotovo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    korisnici: [
+      { 
+        ime: "Ana",
+        godine: 24
+    
+      },
+      {
+        ime: "Ivo",
+        godine: 34  
+      },
+      {
+        ime: "Đuro",
+        godine: 36  
+      }
+    
+    ],
+    proizvoljniText: props.children
+  };
+  }
+  
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+  <p>Prva osoba: {this.state.korisnici[0].ime}, {this.state.korisnici[0].godine} godine, {this.state.proizvoljniText='žensko'}.</p>
+  <p>Druga osoba: {this.state.korisnici[1].ime}, {this.state.korisnici[1].godine} godine, {this.state.proizvoljniText='muško'}.</p>
+  <p>Treća osoba: {this.state.korisnici[2].ime}, {this.state.korisnici[2].godine} godina, {this.state.proizvoljniText='muško'}.</p>
+  </div>
   );
-}
+  }
+  }
 
+  
 export default App;
